04fdc0d77996d3ea9b528487be77773c76591854
i exist <119149121+i-exist74@users.noreply.github.com>
2024-07-24 05:55:28 -0500

    Update mainMenu.mjs

--- modules/mainMenu.mjs
+++ modules/mainMenu.mjs
@@ -7,21 +7,21 @@ const menuButtons = document.querySelectorAll("#main-menu .dropdown__option");
 menuButtons.forEach(btn => {
     switch (btn.value) {
         case "openImportModal":
-            btn.addEventListener("pointerup", e => importModal.showModal());
+            btn.addEventListener("click", e => importModal.showModal());
             break;
         case "openExportModal":
-            btn.addEventListener("pointerup", e => exportModal.showModal());
+            btn.addEventListener("click", e => exportModal.showModal());
             break;
         case "openRenderModal":
-            btn.addEventListener("pointerup", e => renderModal.showModal());
+            btn.addEventListener("click", e => renderModal.showModal());
             break;
         case "openEditDimensionsModal":
-            btn.addEventListener("pointerup", e => editDimensionsModal.showModal());
+            btn.addEventListener("click", e => editDimensionsModal.showModal());
     }
 });
 
 // for mobile - focus button when clicked
-document.addEventListener("pointerup", e => {
+document.addEventListener("click", e => {
     if (
         !e.target.classList.contains("dropdown__button") &&
         !e.target.classList.contains("dropdown__option")
