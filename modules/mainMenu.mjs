21a53eb1ae5066047cfac790b114444da9ca36e6
i exist <119149121+i-exist74@users.noreply.github.com>
2024-07-24 06:44:31 -0500

    Update mainMenu.mjs

--- modules/mainMenu.mjs
+++ modules/mainMenu.mjs
@@ -7,7 +7,7 @@ const menuButtons = document.querySelectorAll("#main-menu .dropdown__option");
 menuButtons.forEach(btn => {
     switch (btn.value) {
         case "openImportModal":
-            btn.addEventListener("click", e => importModal.showModal());
+            btn.addEventListener("click", e => importModal.showModal() + alert(1));
             break;
         case "openExportModal":
             btn.addEventListener("click", e => exportModal.showModal());
