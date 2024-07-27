c0a5ef4d642d7d7626094f583dfff20ae7f4ddce
i exist <119149121+i-exist74@users.noreply.github.com>
2024-07-27 09:21:30 -0500

    Update exportModal.mjs

--- modules/exportModal.mjs
+++ modules/exportModal.mjs
@@ -3,7 +3,13 @@ import Editor from "./Editor.mjs";
 const modal = document.getElementById("export-modal");
 const outputEl = modal.querySelector("textarea");
 const exportButton = document.getElementById("export-project-button");
+const errorDisplay = document.getElementById("export-error-display");
 
 exportButton.addEventListener("click", e => {
-    outputEl.value = Editor.exportProjectData();
+    try {
+        outputEl.value = Editor.exportProjectData();
+    } catch (e) {
+        console.error(e);
+        errorDisplay.textContent = `Export error: ${e}`;
+    }
 });
