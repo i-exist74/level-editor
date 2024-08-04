0518eb8983749fd33a88ff9941b316e87c4981d5
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-04 08:15:20 -0500

    Update exportModal.mjs

--- modules/exportModal.mjs
+++ modules/exportModal.mjs
@@ -8,12 +8,14 @@ const errorDisplay = document.getElementById("export-error-display");
 exportButton.addEventListener("click", e => {
     try {
         const data = Editor.exportProjectData();
-        const blob = new Blob([data], { type: "text/plain" });
+        //const blob = new Blob([data], { type: "text/plain" });
         const elem = document.createElement('a');
-        elem.href = URL.createObjectURL(blob);
+        //elem.href = URL.createObjectURL(blob);
+        elem.href = "data:text/plain;charset=utf-8," + data;
+        elem.target = "_blank";
         //elem.download = filename;        
         document.body.appendChild(elem);
-        elem.click();        
+        elem.click();
         document.body.removeChild(elem);
     } catch (e) {
         console.error(e);
