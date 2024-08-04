a4184d150280a834d9310c9d8d5255397bfc8209
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-04 08:24:09 -0500

    Update exportModal.mjs

--- modules/exportModal.mjs
+++ modules/exportModal.mjs
@@ -2,16 +2,24 @@ import Editor from "./Editor.mjs";
 
 const modal = document.getElementById("export-modal");
 const outputEl = modal.querySelector("textarea");
-const exportButton = document.getElementById("export-project-button");
+const outputToTextareaButton = document.getElementById("output-project-to-textarea-button");
+const downloadButton = document.getElementById("download-project-button");
 const errorDisplay = document.getElementById("export-error-display");
 
-exportButton.addEventListener("click", e => {
+outputToTextareaButton.addEventListener("click", e => {
+    try {
+        outputEl.value = Editor.exportProjectData();
+    } catch (e) {
+        console.error(e);
+        errorDisplay.textContent = `Export error: ${e}`;
+    }
+});
+downloadButton.addEventListener("click", e => {
     try {
         const data = Editor.exportProjectData();
-        //const blob = new Blob([data], { type: "text/plain" });
+        const blob = new Blob([data], { type: "text/plain" });
         const elem = document.createElement('a');
-        //elem.href = URL.createObjectURL(blob);
-        elem.href = "data:text/plain;charset=utf-8," + data;
+        elem.href = URL.createObjectURL(blob);
         elem.target = "_blank";
         //elem.download = filename;        
         document.body.appendChild(elem);
