f656299259d1e1a4d6c69606a52d2cd7cc6338aa
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-05 23:52:20 -0500

    Update Renderer.mjs

--- modules/Renderer.mjs
+++ modules/Renderer.mjs
@@ -136,7 +136,6 @@ function render(levelData, cameraIndex = 0) {
                 // Convert unit square to level coords
                 matrix = m4.scale(matrix, 1, 1, 10);
                 matrix = m4.translate(matrix, 0.5, 0.5, 0.5);
-                matrix = m4.scale(matrix, 1, -1, 1);
                 // Rotation around center of grid space (for floor, ceiling, wall tiles)
                 //matrix = m4.xRotate(matrix, 0);
                 //matrix = m4.yRotate(matrix, 0);
