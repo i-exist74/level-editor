e4370fb7a9291d158aba1b12834197e296d8579d
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-05 21:43:28 -0500

    Update m4.mjs

--- modules/m4.mjs
+++ modules/m4.mjs
@@ -1,5 +1,5 @@
 //i probably got this code from webgl2fundamentals.org
-export default const m4 = {
+export default {
 
     perspective: function (fieldOfViewInRadians, aspect, near, far) {
         var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
