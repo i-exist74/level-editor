083a88f81f64c89cae0996e8fc0f579d4533098e
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-12 04:41:43 -0500

    Update Editor.mjs

--- modules/Editor.mjs
+++ modules/Editor.mjs
@@ -2,28 +2,22 @@ import LevelView from "./LevelView.mjs";
 import { LevelData, Geometry } from "./LevelData.mjs";
 
 const outputContainer = document.getElementById("graphical-display");
+const toolInfoEl = document.getElementById("tool-info");
 
 const levelData = new LevelData();
 const levelView = new LevelView(outputContainer, levelData, { minZoom: 4, maxZoom: 40, zoom: 12 });
 
-/* Camera & display settings */
 
-// Toggle grid - checkbox
 const toggleGridCheckbox = document.getElementById("toggle-grid-input");
 toggleGridCheckbox.addEventListener("change", () => {
     levelView.toggleGrid(toggleGridCheckbox.checked);
 });
 
-
-/* Editing */
-
-// Work layer
 levelView.setWorkLayer(0);
 document.getElementById("work-layer-select").addEventListener("change", e => {
     levelView.setWorkLayer(e.target.value);
 });
 
-// Selection type
 levelView.setSelectionType("paint");
 document.getElementById("selection-type-select").addEventListener("change", e => {
     levelView.setSelectionType(e.target.value);
@@ -37,6 +31,7 @@ document.getElementById("edit-action-select").addEventListener("change", e => {
 });
 document.getElementById("edit-tool-select").addEventListener("change", e => {
     levelView.setGeometryTool(e.target.value);
+    toolInfoEl.textContent = e.target.dataset.description;
 });
 
 
