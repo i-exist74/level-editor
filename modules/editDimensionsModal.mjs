05e01b019a165166d996bb42e03d6ecf1f2c69f8
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-16 20:31:40 -0500

    Update editDimensionsModal.mjs

--- modules/editDimensionsModal.mjs
+++ modules/editDimensionsModal.mjs
@@ -5,6 +5,7 @@ const widthInput = document.getElementById("level-width-input");
 const heightInput = document.getElementById("level-height-input");
 const leftTopButton = document.getElementById("edit-dimensions-left-top");
 const rightBottomButton = document.getElementById("edit-dimensions-right-bottom");
+const errorDisplay = document.getElementById("edit-dimensions-error-display");
 
 leftTopButton.addEventListener("click", e => {
     changeDimensionsAndCloseModal(widthInput.value, heightInput.value, false);
@@ -14,8 +15,13 @@ rightBottomButton.addEventListener("click", e => {
 });
 
 function changeDimensionsAndCloseModal(newWidth, newHeight, rightBottomBorder) {
-    let changedW = levelData.changeWidth(newWidth, rightBottomBorder);
-    let changedH = levelData.changeHeight(newHeight, rightBottomBorder);
-    if (changedW || changedH) levelView.levelDimensionsChanged();
-    modal.close();
+    try {
+        let changedW = levelData.changeWidth(newWidth, rightBottomBorder);
+        let changedH = levelData.changeHeight(newHeight, rightBottomBorder);
+        if (changedW || changedH) levelView.levelDimensionsChanged();
+        modal.close();
+    } catch (e) {
+        console.error(e);
+        errorDisplay.textContent = e.stack;
+    }
 }
