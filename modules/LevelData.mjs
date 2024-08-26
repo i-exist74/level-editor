4ece14b3f5fa275d6be0d489ad162d1776639e86
i exist <119149121+i-exist74@users.noreply.github.com>
2024-08-26 04:53:18 -0500

    Update LevelData.mjs

--- modules/LevelData.mjs
+++ modules/LevelData.mjs
@@ -123,7 +123,7 @@ export const Tiles = (function() {
         try {
             const obj = JSON.parse(str);
             Tiles[currentCategory][obj.nm] = obj;
-            a += `    ,\n`;
+            a += `    ${str},\n`;
         } catch (e) {
             alert(e + " " + str);
             throw e;
